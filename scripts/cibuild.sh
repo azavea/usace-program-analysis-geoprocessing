ced8a5d5b4486f0478be72418589e642f77d4f77
Hector Castro <hcastro@azavea.com>
2016-06-03 15:45:47 -0400

    Use Docker Compose YML that Travis supports

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -3,15 +3,13 @@
 set -e
 set -x
 
-DIR="$(dirname "$0")"
-
 if [ -z "${TRAVIS_TAG}" ]; then
     QUAY_TAG="${TRAVIS_COMMIT:0:7}"
 else
     QUAY_TAG="${TRAVIS_TAG}"
 fi
 
-QUAY_TAG="${QUAY_TAG}" docker-compose -f "${DIR}/../docker-compose.ci.yml" build release
+docker build -t "quay.io/usace/program-analysis-geoprocessing:${QUAY_TAG}" .
 
 docker push "quay.io/usace/program-analysis-geoprocessing:${QUAY_TAG}"
 docker tag -f "quay.io/usace/program-analysis-geoprocessing:${QUAY_TAG}" "quay.io/usace/program-analysis-geoprocessing:latest"
