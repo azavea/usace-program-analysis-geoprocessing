6ec719b5275d194e8bd94231379a892273d54254
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2016-06-06 10:50:15 -0400

    Dockerize and publish to Quay
    Instead of building a JAR which gets included in a Docker image by
    the client repository, we publish a Docker image on Quay which can
    immediately be included by the client.

---
+++ scripts/cibuild.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+if [ -z "${TRAVIS_TAG}" ]; then
+    QUAY_TAG="${TRAVIS_COMMIT:0:7}"
+else
+    QUAY_TAG="${TRAVIS_TAG}"
+fi
+
+docker build -t "quay.io/usace/program-analysis-geoprocessing:${QUAY_TAG}" .
+
+docker push "quay.io/usace/program-analysis-geoprocessing:${QUAY_TAG}"
+docker tag -f "quay.io/usace/program-analysis-geoprocessing:${QUAY_TAG}" "quay.io/usace/program-analysis-geoprocessing:latest"
+docker push "quay.io/usace/program-analysis-geoprocessing:latest"
